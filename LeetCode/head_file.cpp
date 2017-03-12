eaeddf81a5c07f4c3de7001c8dc151d045bcb0c0
Xylong Zhang <zhanghsl.it@foxmail.com>
2017-03-12 17:31:38 +0800

    add head_file.cpp

---
+++ LeetCode/head_file.cpp
@@ -0,0 +1,16 @@
+#define vi vector<int>
+#define pii pair<int,int>
+#define pb push_back
+#define mp make_pair
+#define all(x) x.begin(),x.end()
+#define sz(x) (int)(x.size())
+#define rep(i,a,b) for(int i=a;i<b;i++)
+#define per(i,a,b) for(int i=b-1;i>=a;i--)
+#define inf 1000000007
+#define mod 1000000007
+#define x first
+#define y second
+#define pi acos(-1.0)
+#define ull unsigned long long
+#define ll long long
+#define N 200010
